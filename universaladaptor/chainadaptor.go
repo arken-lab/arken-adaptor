702f36e156868b857c151b47e5d24a90632c556b
Gollum <arken_gollum@protonmail.com>
2021-10-10 14:20:32 +0700

    feat: universal adapotr interfaces

--- universaladaptor/chainadaptor.go
+++ universaladaptor/chainadaptor.go
@@ -1,4 +1,15 @@
 package universaladaptor
 
+type BlockNumber int64
+type LogFilter interface{}
+type Log interface{}
+type TransactionFilter interface{}
+type Transaction interface{}
+type ContractResponse interface{}
+
 type ChainAdaptor interface {
+	GetLatestBlock() (BlockNumber, error)
+	GetLogs(fromBlock int64, toBlock int64, logFilters []LogFilter) ([]Log, error)
+	GetTransactions(fromBlock int64, toBlock int64, transactionFilters []TransactionFilter) ([]Transaction, error)
+	QueryContract(contractQueryData interface{}) (ContractResponse, error)
 }
--- universaladaptor/protocoladaptor.go
+++ universaladaptor/protocoladaptor.go
@@ -1,1 +1,10 @@
 package universaladaptor
+
+type ProtocolAdaptor interface {
+	GetLogFilters() ([]LogFilter, error)
+	GetTransactionFilters() ([]TransactionFilter, error)
+	GetPollingContracts() ([]ContractResponse, error)
+	ProcessLog(log Log) ([]Event, error)
+	ProcessPollingContractResponse(ContractResponse) ([]Event, error)
+	ProcessTransaction(Transaction) ([]Event, error)
+}
